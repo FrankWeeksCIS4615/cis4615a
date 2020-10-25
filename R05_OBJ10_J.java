8fa597429717dac2c3612e20a6a2fe0477fb4447
FrankWeeksCIS4615 <73434337+FrankWeeksCIS4615@users.noreply.github.com>
2020-10-25 16:39:00 -0400

    Update R05_OBJ10_J.java
    Check for integer overflow and disallow either parameter from being the minimum Integer value.

--- R05_OBJ10_J.java
+++ R05_OBJ10_J.java
@@ -1,5 +1,11 @@
-package org.apache.xpath.compiler;
- 
-public class FunctionTable {
-  public static FuncLoader m_functions;
+public static int getAbsAdd(int x, int y) {
+  if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+    throw new IllegalArgumentException();
+  }
+  int absX = Math.abs(x);
+  int absY = Math.abs(y);
+  if (absX > Integer.MAX_VALUE - absY) {
+    throw new IllegalArgumentException();
+  }
+  return absX + absY;
 }
