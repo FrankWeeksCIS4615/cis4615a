ddffb2f97f56ee271696d6cac162be725c7a299f
FrankWeeksCIS4615 <73434337+FrankWeeksCIS4615@users.noreply.github.com>
2020-10-25 16:54:24 -0400

    Update R05_OBJ10_J.java
    This declares FuncLoader as final and treats it as constant. Additionally, this commit fixes a wrong commit.

--- R05_OBJ10_J.java
+++ R05_OBJ10_J.java
@@ -1,11 +1,6 @@
-public static int getAbsAdd(int x, int y) {
-  if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
-    throw new IllegalArgumentException();
-  }
-  int absX = Math.abs(x);
-  int absY = Math.abs(y);
-  if (absX > Integer.MAX_VALUE - absY) {
-    throw new IllegalArgumentException();
-  }
-  return absX + absY;
+package org.apache.xpath.compiler;
+ 
+public class FunctionTable {
+  public static final FuncLoader m_functions;
+  // Initialize m_functions in a static initialization block
 }
